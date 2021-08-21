f314953b958511a5e4c853a0ef2e39aeae58d58b
todor-grigorov <t.grigorov99@gmail.com>
2021-08-22 00:18:42 +0300

    Added Toolbar and Tabs in Home Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAPi29H9v7nW1mPkqRvPkOIzOsBHJpptcg",
+    authDomain: "car-sales-3dc8a.firebaseapp.com",
+    projectId: "car-sales-3dc8a",
+    storageBucket: "car-sales-3dc8a.appspot.com",
+    messagingSenderId: "948513836371",
+    appId: "1:948513836371:web:1f2ef350b58336cf9365a6"
+  }
 };
 
 /*
