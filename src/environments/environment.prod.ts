a47921261b09d0d70f0f4d3436e26deb5f13e6b3
t.grigorov <t.grigorov99@gmail.com>
2022-12-14 15:04:24 +0100

    updated packages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAPi29H9v7nW1mPkqRvPkOIzOsBHJpptcg",
+    authDomain: "car-sales-3dc8a.firebaseapp.com",
+    projectId: "car-sales-3dc8a",
+    storageBucket: "car-sales-3dc8a.appspot.com",
+    messagingSenderId: "948513836371",
+    appId: "1:948513836371:web:1f2ef350b58336cf9365a6"
+  }
 };
